cf93e754ec8f586e7e74583494b39eabcfb6a29b
stks56 <t.suenaga428@gmail.com>
2019-03-05 00:32:57 +0900

    posts編集削除機能の追加、posts/showの修正、user model追加、etc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,45 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      title: params[:title],
+      content: params[:content],
+      game_id: params[:game_id],
+      user_id: "1"
+    )
+    if @post.save
+      flash[:notice] = "記事を投稿しました"
+      redirect_to("/posts")
+    else
+      render("posts/new")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "記事を編集しました"
+      redirect_to("/posts/#{@post.id}")
+    else
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'games/:game_name' => 'games#show'
 
   get 'posts' => 'posts#index'
+  get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/create' => 'posts#create'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
 
   get '/' => 'home#top'
   get 'about' => 'home#about'
