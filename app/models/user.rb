99f2d653e9fdefb0389d2247d9491ef1530cdc30
stks56 <t.suenaga428@gmail.com>
2019-04-19 22:06:24 +0900

    posts関連の統合テストを記述 #28、gitignore DS_Storeの設定漏れを修正、不要なtestフォルダを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true}
-  validates :image, {presence: true}
 
   mount_uploader :image, ImageUploader
 
