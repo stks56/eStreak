3d4317ebd4fd94e5c4aa78781770d54cf1aced6b
stks56 <t.suenaga428@gmail.com>
2019-04-29 10:12:33 +0900

    #32 ユーザ編集ページにユーザー画像編集機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      added_attrs = [:name, :image]
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+      devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
     end
 end
