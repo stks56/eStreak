390f84e3d5e667ece4fa2d620ffc9b24576bd774
stks56 <t.suenaga428@gmail.com>
2019-05-02 06:52:06 +0900

    user詳細ページにページネーション実装、postのform周り修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id).page(params[:page]).per(10).order("created_at desc")
   end
 end
