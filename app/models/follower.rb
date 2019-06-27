06da780139c6c448f3114e848effb75e9c885a1a
stks56 <t.suenaga428@gmail.com>
2019-06-27 09:34:14 +0900

    like, followerモデルに一意性を追加

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,5 +1,5 @@
 class Follower < ApplicationRecord
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: {scope: :follower_id}
   validates :follower_id, presence: true
 
   belongs_to :user
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  validates :post_id, presence: true
+  validates :post_id, presence: true, uniqueness: {scope: :user_id}
   validates :user_id, presence: true
 
   belongs_to :post
