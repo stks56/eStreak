8ca59d5fb2bb8d7bf25fcabc043979510575e710
stks56 <t.suenaga428@gmail.com>
2019-07-19 08:36:59 +0900

    add depemndent option to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_one :game_image, dependent: :destroy
   accepts_nested_attributes_for :game_image, allow_destroy: true
 end
