2bd10142f513100711047a3d5ec298a89e3dc6cc
stks56 <t.suenaga428@gmail.com>
2019-07-21 10:08:00 +0900

    Remove unnecessary logic　from game#show action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,11 +10,6 @@ class GamesController < ApplicationController
                  .per(10)
                  .order('created_at desc')
                  .includes(:user, :game)
-    like_ranks = Like.where(game_id: @game.id)
-                     .group(:post_id)
-                     .order(Arel.sql('count(post_id) desc'))
-                     .limit(5)
-                     .pluck(:post_id)
     @posts_rank = Post.where(game_id: @game.id).order('likes_count desc').limit(3)
     @twitch_streams = TwitchApi.get_streams_with_cache(@game.name, 8)
   end
