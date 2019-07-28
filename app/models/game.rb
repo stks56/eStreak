a134c129a59f9f02547c51d9c10b97e098b73079
stks56 <t.suenaga428@gmail.com>
2019-07-28 16:14:20 +0900

    add delegate method and fix vioration Law of Demeter.

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,7 @@ class Game < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_one :game_image, dependent: :destroy
   accepts_nested_attributes_for :game_image, allow_destroy: true
+
+  delegate :icon_image, to: :game_image
+  delegate :background_image, to: :game_image
 end
